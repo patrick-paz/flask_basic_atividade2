7c030e4b2fd98c775ac652c9702edbaca8ca6291
patrick-paz <patrick.f.g.p@gmail.com>
2022-01-25 21:20:16 -0300

    Initial Commit

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ def hello(name):
     today = datetime.now()
     data_local = today.strftime("%d/%m/%y - %H:%M:%S")
     return render_template("hello.html", name=name, today=data_local)
-
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
